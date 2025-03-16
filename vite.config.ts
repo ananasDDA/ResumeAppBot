676330cb2dc5440b526d1a3c8dc072131c482198
daniil <daniil@MacBook-Air-Daniil.local>
2025-03-16 21:32:04 +0300

    pages fix 9+

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,6 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { resolve } from 'path'
 import fs from 'fs'
 
 // https://vitejs.dev/config/
@@ -7,15 +8,22 @@ export default defineConfig({
   plugins: [
     react(),
     {
-      name: 'create-nojekyll',
-      writeBundle() {
-        fs.writeFileSync('./docs/.nojekyll', '');
+      name: 'create-nojekyll-and-cname',
+      closeBundle() {
+        const outDir = resolve(__dirname, 'docs');
+        fs.writeFileSync(resolve(outDir, '.nojekyll'), '');
+        // Also copy 404.html to the output directory
+        if (fs.existsSync(resolve(__dirname, 'public', '404.html'))) {
+          fs.copyFileSync(
+            resolve(__dirname, 'public', '404.html'),
+            resolve(outDir, '404.html')
+          );
+        }
       }
     }
   ],
   build: {
-    outDir: './docs',
-    sourcemap: true
+    outDir: 'docs'
   },
   base: '/ResumeAppBot/'
 });
